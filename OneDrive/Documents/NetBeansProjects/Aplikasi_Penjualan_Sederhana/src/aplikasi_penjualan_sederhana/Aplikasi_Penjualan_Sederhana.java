6cb2349738c7f1f71ab859891c52b5ec58fc74d4
KrisnaRizki45 <krisna.rizkydermawan@gmail.com>
2024-05-20 11:38:35 +0700

    mantap

---
+++ OneDrive/Documents/NetBeansProjects/Aplikasi_Penjualan_Sederhana/src/aplikasi_penjualan_sederhana/Aplikasi_Penjualan_Sederhana.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplikasi_penjualan_sederhana;
+
+/**
+ *
+ * @author krisn
+ */
+public class Aplikasi_Penjualan_Sederhana {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //koneksi k = new koneksi();
+        //k.connect();
+        
+        Login l = new Login();
+        l.setVisible(true);
+    }
+    
+}
